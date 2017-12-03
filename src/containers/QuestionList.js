8962c94a07c6b9cb32177a886f8d156322870f6d
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-03 13:18:21 -0700

    Connect questionlist to state

--- src/containers/QuestionList.js
+++ src/containers/QuestionList.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Question } from './Question';
+import { connect } from 'react-redux';
+import { List } from 'immutable';
+import Question from './Question';
 
 export class QuestionList extends React.Component {
   render() {
-    const questions = this.props.questions.map((question, index) => {
+    const questions = this.props.questions.map(question => {
       return (
-        <li key={index}>
-          <Question info={question} />
+        <li key={question.id}>
+          <Question qId={question.id} />
         </li>
       );
     });
     return <ul>{questions}</ul>;
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    questions: state.get('questions'),
+  };
+}
+
+export default connect(mapStateToProps)(QuestionList);
