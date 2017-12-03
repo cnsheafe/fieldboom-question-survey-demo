614322b75b9ea5bc99a258e5dc2039755b4b7802
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-03 14:34:55 -0700

    Enable drag-n-drop

--- src/containers/Choice.js
+++ src/containers/Choice.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { DragSource } from 'react-dnd';
+import PropTypes from 'prop-types';
 import { AddQuestion } from '../connectors/redux/action-creators/QuestionAC';
 
+const cardSource = {
+  beginDrag(props) {
+    return {
+      title: props.title,
+    };
+  },
+};
 export class Choice extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    connectDragSource: PropTypes.func,
+    isDragging: PropTypes.bool,
+  };
   render() {
+    if (this.props.connectDragSource) {
+      return this.props.connectDragSource(
+        <div>
+          {this.props.title}
+          <button
+            onClick={e => {
+              this.props.addDefaultQuestion();
+            }}
+          >
+            ClickMe!
+          </button>
+        </div>
+      );
+    }
     return (
       <div>
         {this.props.title}
@@ -19,7 +47,6 @@ export class Choice extends React.Component {
   }
 }
 
-
 function mapDispatchToProps(dispatch) {
   return {
     addDefaultQuestion: () => {
@@ -28,4 +55,11 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(null, mapDispatchToProps)(Choice);
+// @DragSource('Choice', cardSource, (connect, monitor) => ({
+//   connectDragSource: connect.dragSource(),
+//   isDragging: monitor.isDragging(),
+// }))
+export default DragSource('Choice', cardSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging(),
+}))(connect(null, mapDispatchToProps)(Choice));
