d7cfb291380d61bfaaa7ee71910150999d01aa87
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-03 13:14:04 -0700

    Add redux-connector to component
    Map a dispatch for a default question whever the test button is clicked.

--- src/containers/Choice.js
+++ src/containers/Choice.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { AddQuestion } from '../connectors/redux/action-creators/QuestionAC';
 
 export class Choice extends React.Component {
   render() {
     return (
       <div>
         {this.props.title}
+        <button
+          onClick={e => {
+            this.props.addDefaultQuestion();
+          }}
+        >
+          ClickMe!
+        </button>
       </div>
-    )
+    );
   }
 }
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addDefaultQuestion: () => {
+      dispatch(AddQuestion('My Question', ['a', 'b', 'c']));
+    },
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Choice);
