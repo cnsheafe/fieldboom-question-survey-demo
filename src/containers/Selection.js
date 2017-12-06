e2ce3d89cb375773ca3cc8a7301c232c349584f4
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-06 12:14:22 -0700

    Replace selection root tag with section
    Also add tags for logo and go-back.

--- src/containers/Selection.js
+++ src/containers/Selection.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Choice from './Choice';
+import '../styles/selection.css';
+import logoImg from '../assets/favicon.png';
+
+const uuid = require('uuid/v4');
 
 export class Selection extends React.Component {
   constructor(props) {
@@ -7,13 +11,25 @@ export class Selection extends React.Component {
     this.choices = props.choices;
   }
   render() {
-    const listOfChoices = this.choices.map((choice, index) => {
+    const listOfChoices = this.choices.map(choice => {
       return (
-        <li key={index}>
+        <li key={uuid()}>
           <Choice title={choice.title} icon={choice.icon} />
         </li>
       );
     });
-    return <nav>{listOfChoices}</nav>;
+    return (
+      <section className="selection-of-choices">
+        <div>
+          <span><img src={logoImg} alt="Fieldboom icon" /></span>
+          <h2>Fieldboom</h2>
+        </div>
+        <div>
+          <span>arrow icon</span>
+          <span className="back-to-forms">BACK TO YOUR FORMS</span>
+        </div>
+        <ul>{listOfChoices}</ul>
+      </section>
+    );
   }
 }
