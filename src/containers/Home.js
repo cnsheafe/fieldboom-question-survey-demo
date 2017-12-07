11d1f03bb7ef8f47893fb02d39676a8af3020c53
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-06 20:21:31 -0700

    Replace Canvas with connected version

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 import { Selection } from './Selection';
-import { Canvas } from './Canvas';
+import Canvas from './Canvas';
 import Edit from './Edit';
 import '../styles/home.css';
 
