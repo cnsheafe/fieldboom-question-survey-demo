2be63dd3468b332b72e80f143d97e7a4dec303df
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-06 19:44:30 -0700

    Refactor jsx and remove redux connect
    Component is now completely drag-n-drop and no longer needs redux.
    JSX refactored for simpler rendering code.

--- src/containers/Choice.js
+++ src/containers/Choice.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { DragSource } from 'react-dnd';
 import PropTypes from 'prop-types';
-import { AddQuestion } from '../connectors/redux/action-creators/question-creator';
-import { ChangeQuestion } from '../connectors/redux/action-creators/current-question-editor';
 import '../styles/choice.css';
 
 export class Choice extends React.Component {
@@ -11,46 +8,24 @@ export class Choice extends React.Component {
     title: PropTypes.string.isRequired,
     connectDragSource: PropTypes.func,
     isDragging: PropTypes.bool,
-    addDefaultQuestion: PropTypes.func,
   };
   render() {
-    if (this.props.connectDragSource) {
-      return this.props.connectDragSource(
-        <div>
-          <h1>{this.props.title}</h1>
-          <button
-            onClick={e => {
-              this.props.addDefaultQuestion();
-            }}
-          >
-            ClickMe!
-          </button>
-        </div>
-      );
-    }
-    return (
+    const jsxElm = (
       <div className="selection-choice">
-        <h1>{this.props.title}</h1>
-        <button
-          onClick={e => {
-            this.props.addDefaultQuestion();
-          }}
-        >
-          ClickMe!
-        </button>
+        <div className="icon">
+          <i className="material-icons white-icon">{this.props.icon}</i>
+        </div>
+        <h3>{this.props.title}</h3>
       </div>
     );
+
+    if (this.props.connectDragSource) {
+      return this.props.connectDragSource(jsxElm);
+    }
+    return jsxElm;
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    addDefaultQuestion: () => {
-      const action = dispatch(AddQuestion('My Question', ['a', 'b', 'c']));
-      dispatch(ChangeQuestion(action.id));
-    },
-  };
-}
 const cardSource = {
   beginDrag(props) {
     return {
@@ -62,4 +37,4 @@ const cardSource = {
 export default DragSource('Choice', cardSource, (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging(),
-}))(connect(null, mapDispatchToProps)(Choice));
+}))(Choice);
