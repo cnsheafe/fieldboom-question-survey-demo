2586208738dff987af678184aa59a36c6a9d4e7b
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-07 12:06:37 -0700

    Remove unused action types

--- src/connectors/redux/action-creators/current-question-editor.js
+++ src/connectors/redux/action-creators/current-question-editor.js
@@ -1,8 +1,4 @@
 export const CHANGE_QUESTION = 'CHANGE_QUESTION';
-export const EDIT_TITLE = 'EDIT_TITLE';
-export const ADD_ANSWER = 'ADD_ANSWER';
-export const REMOVE_ANSWER = 'REMOVE_ANSWER';
-export const CHANGE_ANSWER = 'CHANGE_ANSWER';
 export const HOVER_ON_QUESTION = 'HOVER_QUESTION';
 export const CANCEL_HOVER = 'CANCEL_HOVER_QUESTION';
 
@@ -14,6 +10,7 @@ export function ChangeQuestion(qId) {
   }
   return action;
 }
+
 
 export function HoverOver(qId) {
   const action = {
