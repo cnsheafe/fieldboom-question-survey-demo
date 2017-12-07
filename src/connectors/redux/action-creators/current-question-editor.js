142c9daf4261da8d03d54f652a018302dd5ebdd8
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-06 22:09:08 -0700

    Add hover feature for drag and drop on questions

--- src/connectors/redux/action-creators/current-question-editor.js
+++ src/connectors/redux/action-creators/current-question-editor.js
@@ -3,12 +3,29 @@ export const EDIT_TITLE = 'EDIT_TITLE';
 export const ADD_ANSWER = 'ADD_ANSWER';
 export const REMOVE_ANSWER = 'REMOVE_ANSWER';
 export const CHANGE_ANSWER = 'CHANGE_ANSWER';
+export const HOVER_ON_QUESTION = 'HOVER_QUESTION';
+export const CANCEL_HOVER = 'CANCEL_HOVER_QUESTION';
 
 
 export function ChangeQuestion(qId) {
   const action = {
     type: CHANGE_QUESTION,
     id: qId,
+  }
+  return action;
+}
+
+export function HoverOver(qId) {
+  const action = {
+    type: HOVER_ON_QUESTION,
+    id: qId,
+  }
+  return action;
+}
+
+export function CancelHoverOver() {
+  const action = {
+    type: CANCEL_HOVER,
   }
   return action;
 }
--- src/connectors/redux/reducers/current-question-reducer.js
+++ src/connectors/redux/reducers/current-question-reducer.js
@@ -1,16 +1,29 @@
 import {
   CHANGE_QUESTION,
+  HOVER_ON_QUESTION,
+  CANCEL_HOVER,
 } from '../action-creators/current-question-editor';
 
 function changeQuestion(state, action) {
   return state.set('id', action.id);
 }
 
+function hoverOver(state, action) {
+  return state.set('next', action.id);
+}
+
+function cancelHoverOver(state, action) {
+  return state.set('next', '');
+}
 
 export default function(state, action = {}) {
   switch (action.type) {
     case CHANGE_QUESTION:
       return changeQuestion(state, action);
+    case HOVER_ON_QUESTION:
+      return hoverOver(state, action);
+    case CANCEL_HOVER:
+      return cancelHoverOver(state, action);
     default:
       return state;
   }
