dfb20618883755febc4da43a62f0470399ad5bac
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-07 23:27:14 -0700

    Add comments to action creators

--- src/connectors/redux/action-creators/current-question.js
+++ src/connectors/redux/action-creators/current-question.js
@@ -3,6 +3,10 @@ export const HOVER_ON_QUESTION = 'HOVER_QUESTION';
 export const CANCEL_HOVER = 'CANCEL_HOVER_QUESTION';
 
 
+/**
+ * Create action to change current question to edit
+ * @param {string} qId Question Id
+ */
 export function ChangeQuestion(qId) {
   const action = {
     type: CHANGE_QUESTION,
@@ -11,7 +15,10 @@ export function ChangeQuestion(qId) {
   return action;
 }
 
-
+/**
+ * Create action to highlight specific question
+ * @param {string} qId Question id
+ */
 export function HoverOver(qId) {
   const action = {
     type: HOVER_ON_QUESTION,
@@ -20,6 +27,9 @@ export function HoverOver(qId) {
   return action;
 }
 
+/**
+ * Create action to clear the focused question
+ */
 export function CancelHoverOver() {
   const action = {
     type: CANCEL_HOVER,
--- src/connectors/redux/action-creators/question-list.js
+++ src/connectors/redux/action-creators/question-list.js
@@ -7,6 +7,11 @@ export const DELETE_QUESTION = 'DELETE_QUESTION';
 export const MOVE_QUESTION = 'MOVE_QUESTION';
 export const EDIT_QUESTION = 'EDIT_QUESTION';
 
+/**
+ * Adds question to the list
+ * @param {string} text The question
+ * @param {List<string>} answers List of possible answers
+ */
 export function AddQuestion(text, answers) {
   const action = {
     type: ADD_QUESTION,
@@ -18,14 +23,25 @@ export function AddQuestion(text, answers) {
   return action;
 }
 
+/**
+ * Deletes question from the list
+ * @param {string} qId Question Id
+ */
 export function DeleteQuestion(qId) {
   const action = {
     type: DELETE_QUESTION,
     id: qId,
   };
+
   return action;
 }
 
+/**
+ * Edit a question on the list
+ * @param {string} text New text
+ * @param {string} qId Id of the question
+ * @param {List<string>} answers New answers
+ */
 export function EditQuestion(text, qId, answers) {
   const action = {
     type: EDIT_QUESTION,
@@ -33,9 +49,15 @@ export function EditQuestion(text, qId, answers) {
     title: text,
     answers: List(answers),
   };
+
   return action;
 }
 
+/**
+ * Moves a question on the list to a new index
+ * @param {string} sourceId Id of the question to move
+ * @param {*} index Index to move the question to
+ */
 export function MoveQuestion(sourceId, index) {
   const action = {
     type: MOVE_QUESTION,
