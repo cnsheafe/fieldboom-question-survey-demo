007cedfcadd1f1cb057ced5a25a6cabeb51cfbb7
Christofer Sheafe <cnsheafe@gmail.com>
2017-12-08 12:29:39 -0700

    Add render test via react-dom

--- src/containers/__test__/QuestionList.test.js
+++ src/containers/__test__/QuestionList.test.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { List } from 'immutable';
+import ReactDOM from 'react-dom';
 import { QuestionList } from '../QuestionList';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('QuestionList', () => {
+  test('should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<QuestionList questions={List()} />, div);
+  })
   test('should render a list', () => {
     const wrapper = Enzyme.shallow(<QuestionList questions={List()} />);
     expect(wrapper.find('ul')).toHaveLength(1);
